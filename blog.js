185d08ec26c15c80dc8d153e370c07e789538177
baooab <3183442656@qq.com>
2016-12-08 14:29:42 +0800

    新增 用 Express 编写的简易网站

--- blog.js
+++ blog.js
@@ -1,37 +1,35 @@
-var http = require('http'),
-fs = require('fs');
+var express = require('express');
+var app = express();
+// 设置 handlebars 视图引擎
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
 
-function serveStaticFile(res, path, contentType, responseCode) {
-    if(!responseCode) responseCode = 200;
-    fs.readFile(__dirname + path, function(err,data) {
-        if(err) {
-            res.writeHead(500, { 'Content-Type': 'text/plain' });
-            res.end('500 - Internal Error');
-        } else {
-            res.writeHead(responseCode, { 'Content-Type': contentType });
-            res.end(data);
-        }
-    });
-}
+app.use(express.static(__dirname + '/public'));
 
-http.createServer(function(req,res) {
-    // 规范化 url，去掉查询字符串、可选的反斜杠，并把它变成小写
-    var path = req.url.replace(/\/?(?:\?.*)?$/, '').toLowerCase();
-    switch(path) {
-        case '':
-            serveStaticFile(res, '/public/home.html', 'text/html');
-            break;
-        case '/about':
-            serveStaticFile(res, '/public/about.html', 'text/html');
-            break;
-        case '/img/logo':
-            serveStaticFile(res, '/public/img/logo.jpg',
-            'image/jpeg');
-            break;
-        default:
-            serveStaticFile(res, '/public/404.html', 'text/html', 404);
-        break;
-    }
-}).listen(3000);
+app.get('/', function(req, res) {
+    res.render('home');
+});
 
-console.log('服务器运行在 localhost:3000; 键入 Ctrl-C 终止服务....');
+app.get('/about', function(req, res) {
+    res.render('about');
+});
+
+// 定制 404 页面
+app.use(function(req, res) {
+    res.status(404);
+    res.render('404');
+});
+
+// 定制 500 页面
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+    console.log( '使用了 Express 服务启动在 http://localhost:' +
+    app.get('port') + '; 键入 Ctrl-C 终止服务。' );
+});
